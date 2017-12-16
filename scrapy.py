1b27490ee9e2cd4b934f3f049400d73b6bda2def
Matt Toothman <mtoothman@users.noreply.github.com>
2017-12-16 11:04:59 -0500

    handle success and failure

--- scrapy.py
+++ scrapy.py
@@ -3,41 +3,30 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-target = "http://cd1025.com/about/playlists/now-playing"
-page = urllib2.urlopen(target)
+# target = "http://cd1025.com/about/playlists/now-playing"
+# page = urllib2.urlopen(target)
 
-soup = BeautifulSoup(page, 'html.parser')
+# soup = BeautifulSoup(page, 'html.parser')
 
-negative = [
-    DEPECHE MODE
-    MASTER AND SERVANT
-
-    FRANK TURNER
-    RECOVERY
-
-    DAVID BOWIE
-    FIVE YEARS
-    ]
-
-positive = [
-    VIOLENT FEMMES
-    BLISTER IN THE SUN
-
-    FRANK TURNER
-    RECOVERY
-
-    DAVID BOWIE
-    FIVE YEARS
-    ]
+negative = [ "DEPECHE MODE","MASTER AND SERVANT", "DAVID BOWIE", "FIVE YEARS" ]
+positive = [ "VIOLENT FEMMES", "BLISTER IN THE SUN", "FRANK TURNER", "RECOVERY" ]
 
 def artist_title():
     result = []
-#    for i in soup.find_all('tr'):
-    for i in negative:
+    for i in soup.find_all('tr'):
         result.append(i.text)
         if "VIOLENT FEMMES" in i.text:
             print i.text
             print "found!"
+    else:
+        print "not found!"
+
+def check(input):
+    result = []
+    for i in input:
+        result.append(i)
+    if "VIOLENT FEMMES" in result:
+        print "found!"
     else:
         print "not found!"
 
@@ -47,6 +36,11 @@ def all_times():
         result.append(i.text)
     return result
 
-artist_title()
-all_times()
+print("positive...")
+check(positive)
+
+print("negative...")
+check(negative)
+#artist_title()
+#all_times()
 print("OK")
